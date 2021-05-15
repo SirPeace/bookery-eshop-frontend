4bbde8d08e46b5875184131195d735de22b414ae
SirPeace <roman.khabibulin12@gmail.com>
2021-05-15 14:07:09 +0300

    Initialize Next app with Storybook, TypeScript and Sass modules

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+        },
+      },
+    },
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+  ],
+}
